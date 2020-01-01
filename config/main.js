804ad90fcb68785084a1967d82d5991d36c8eb4f
0xFaisal <pd9@outlook.sa>
2020-01-01 19:21:53 +0300

    Add gamestatus on config

--- config/main.js
+++ config/main.js
@@ -1,12 +1,13 @@
 
 var config = { 
 bot: { 
-           "token_bot": "TOKEN BOT",
-           "owners": [  ],
-           "botid": "BOT ID",
-           "supporter":  [ ],
-           "prefix": "#",
-           "premiumlobbyserverid": ""
+    "token_bot": "TOKEN BOT",
+    "owners": [  ],
+    "botid": "BOT ID",
+    "supporter":  [ ],
+    "prefix": "#",
+    "premiumlobbyserverid": "",
+    "gamestatus": "[membercount] User Online!" // You can use [servercount] too!
 }
 
 }
